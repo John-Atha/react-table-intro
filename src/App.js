a197162ec32262357ad952094d884146f7eeba2d
John-Atha <giannisj3@gmail.com>
2021-12-10 12:49:01 +0200

    Test title upd

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Table from './Table';
 function App() {
   return (
     <div className="App">
-      <h1>React-table intro</h1>
+      <h1>React-table intro testing branch</h1>
       <Table />
     </div>
   );
